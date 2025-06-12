8f9c8107639c2fda94ca948094bf270f0c0faefa
matywastaken <matu.sanchez.225@gmail.com>
2025-06-12 16:22:47 -0300

    Primera versión pantalla de inicio

--- App.js
+++ App.js
@@ -5,13 +5,15 @@ import HomeScreen from "./Screens/HomeScreen";
 import RankingScreen from "./Screens/RankingScreen";
 import LoginScreen from "./Screens/LoginScreen";
 import GameScreen from './src/screens/GameScreen';
+import RegistroLoginScreen from './Screens/RegistroLoginScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-   <NavigationContainer>
-      <Stack.Navigator initialRouteName="Inicio">
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="RegistroLoginScreen">
+        <Stack.Screen name="RegistroLoginScreen" component={RegistroLoginScreen} options={{ headerTitle: "Proyecto PONG" }} />
         <Stack.Screen name="Inicio" component={HomeScreen} />
         <Stack.Screen name="Ranking" component={RankingScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
