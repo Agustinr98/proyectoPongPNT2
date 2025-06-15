99a43ab82863d43afa946f2b061b87a9ddb4e60e
Agustinr98 <agustin.ruiz.98@hotmail.com>
2025-06-15 14:19:13 -0300

    Actualización de navegación y leaderboard screen

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from "./Screens/HomeScreen";
-import RankingScreen from "./Screens/RankingScreen";
-import LoginScreen from "./Screens/LoginScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+import RegistroLoginScreen from "./src/screens/RegistroLoginScreen";
+import LeaderBoard from "./src/screens/LeaderBoard";
+import LoginScreen from "./src/screens/LoginForm";
 import GameScreen from './src/screens/GameScreen';
-import RegistroLoginScreen from './Screens/RegistroLoginScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,7 +15,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="RegistroLoginScreen">
         <Stack.Screen name="RegistroLoginScreen" component={RegistroLoginScreen} options={{ headerTitle: "Proyecto PONG" }} />
         <Stack.Screen name="Inicio" component={HomeScreen} />
-        <Stack.Screen name="Ranking" component={RankingScreen} />
+        <Stack.Screen name="Ranking" component={LeaderBoard} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Game" component={GameScreen} options={{ headerShown: false }} />
       </Stack.Navigator>
